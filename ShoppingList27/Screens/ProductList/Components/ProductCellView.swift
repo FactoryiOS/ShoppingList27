8ee3b21e0ae9a12f4269231ed6aca35baa0bdf00
Hajime4life <doguziagerman@gmail.com>
2025-09-10 20:02:05 +0300

    [Feature] Added ProductCellView for ProductList

---
+++ ShoppingList27/Screens/ProductList/Components/ProductCellView.swift
@@ -0,0 +1,51 @@
+//
+//  ProductCellView.swift
+//  ShoppingList27
+//
+//  Created by Hajime4Life on 10.09.2025.
+//
+
+import SwiftUI
+
+struct ProductCellView: View {
+    
+    var product: ProductCellModel
+    
+    @Binding var isChecked: Bool
+
+    var body: some View {
+        HStack {
+            Button("", action: { isChecked.toggle() })
+            .buttonStyle(.checkbox(isChecked: isChecked))
+            
+            Text(product.name)
+            Spacer()
+            Text("\(product.count) \(product.unitMeasure.shortName)")
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var isChecked1 = false
+    @Previewable @State var isChecked2 = false
+    @Previewable @State var isChecked3 = false
+    
+    let productCellPreview = VStack(alignment: .leading, spacing: 20) {
+        ProductCellView(product: .mock1, isChecked: $isChecked1)
+            .padding(.horizontal)
+        Divider()
+        ProductCellView(product: .mock2, isChecked: $isChecked2)
+            .padding(.horizontal)
+        Divider()
+        ProductCellView(product: .mock3, isChecked: $isChecked3)
+            .padding(.horizontal)
+    }
+    .frame(maxHeight: .infinity)
+    .background(Color.backgroundScreen)
+    
+    productCellPreview
+        .colorScheme(.light)
+    
+    productCellPreview
+        .colorScheme(.dark)
+}
