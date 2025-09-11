60f9d526e26eb01e3de026907a96577cc1649d9c
Vladimir <bizin.vladimir01@gmail.com>
2025-09-11 10:11:40 +0300

    [Feature] Add ListsView

---
+++ ShoppingList27/Screens/MainView/ListsView.swift
@@ -0,0 +1,124 @@
+//
+//  ListsView.swift
+//  ShoppingList27
+//
+//  Created by Vladimir on 11.09.2025.
+//
+
+import SwiftUI
+
+struct ListsMainView: View {
+    
+    @ObservedObject private var viewModel: ListsViewModel
+    @State private var isCreatingNewList: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color.backgroundScreen
+                    .ignoresSafeArea()
+                content
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    titleView
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    settingsMenu
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private var content: some View {
+        ZStack(alignment: .bottom) {
+            if viewModel.shouldDisplayPlaceholder {
+                placeholderView
+                    .frame(maxHeight: .infinity)
+            } else {
+                listsScrollView
+            }
+            BaseButton(title: Strings.createList,
+                       action: {
+                print("CreatingNewList")
+                isCreatingNewList = true
+            })
+            .padding(.horizontal, 16)
+            .padding(.bottom, 20)
+        }
+    }
+    
+    private var placeholderView: some View {
+        Text("Placeholder")
+    }
+    
+    private var listsScrollView: some View {
+        ScrollView {
+            LazyVStack(alignment: .center, spacing: 12) {
+                ForEach(viewModel.lists) { list in
+                    ListItemView(item: list)
+                        .padding(.bottom, bottomPaddingForRow(list: list))
+                }
+                Spacer()
+            }
+            .padding(.horizontal, 16)
+            .padding(.top, 12)
+        }
+    }
+    
+    private var titleView: some View {
+        Text(Strings.title)
+            .font(Fonts.semiBold28)
+            .foregroundStyle(Color.grey80)
+    }
+    
+    private var settingsMenu: some View {
+        Menu("SettingsMenu",
+             systemImage: ImageTitles.settingsMenu,
+             content: { })
+        .tint(Color.grey80)
+    }
+    
+    private func bottomPaddingForRow(list: ListItem) -> CGFloat {
+        list.id == viewModel.lists.last?.id ? 56 : 0
+    }
+    
+    init(viewModel: ListsViewModel) {
+        self.viewModel = viewModel
+    }
+    
+}
+
+private extension ListsMainView {
+    enum Strings {
+        static let title = "Мои Списки"
+        static let createList = "Создать список"
+    }
+    enum ImageTitles {
+        static let settingsMenu = "ellipsis.circle"
+    }
+    enum Fonts {
+        static let semiBold28: Font = .system(size: 28,
+                                              weight: .semibold)
+    }
+}
+
+#Preview("Data") {
+    let viewModel = ListsViewModel()
+    viewModel.insert(list: .mock)
+    viewModel.insert(list: .mock2)
+    viewModel.insert(list: .mock3)
+    viewModel.insert(list: .mock)
+    viewModel.insert(list: .mock2)
+    viewModel.insert(list: .mock3)
+    viewModel.insert(list: .mock)
+    viewModel.insert(list: .mock2)
+    viewModel.insert(list: .mock3)
+    return ListsMainView(viewModel: viewModel)
+}
+
+#Preview("Empty") {
+    let viewModel = ListsViewModel()
+    return ListsMainView(viewModel: viewModel)
+}
